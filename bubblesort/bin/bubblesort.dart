f1487806cb1d55e50536d458e855aa976c3ed512
Akhil Jose <124168112+akhil9645@users.noreply.github.com>
2023-05-28 12:47:25 +0530

    Add files via upload

---
+++ bubblesort/bin/bubblesort.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  List<int> array = [12, 4, 55, 11, 33, 6];
+  for (int i = 0; i < array.length - 1; i++) {
+    for (int j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  print(array);
+}
